de2fd2bbee3c31ef83b75eba1dd2889296a50c19
Beyza Tozman <tozmanbeyza1@gmail.com>
2023-10-23 22:09:15 +0300

    resolved the not loading issue and made the '!' functionality.

--- src/DocsLinkedList.java
+++ src/DocsLinkedList.java
@@ -3,8 +3,13 @@ public class DocsLinkedList {
     int length;
 
     public void addNewDocNode(String docName){
+        //docName=docName.toLowerCase(); bu satır çok saçma bi şekilde çok bozuyor
         DocsNode newDocNode=new DocsNode(docName);
-        if(head==null) head= newDocNode;
+        if(head==null) {
+            head= newDocNode;
+            length++;
+            return;
+        }
         DocsNode walk=head;
         while(walk.nextDoc!=null){
             walk=walk.nextDoc;
@@ -14,10 +19,12 @@ public class DocsLinkedList {
     }
     public void removeDocNode(String docName){
         if(head==null) return;
+        docName=docName.toLowerCase();
         DocsNode walk=head;
         DocsNode prev=head;
         int index=0;
         while(walk!=null){
+            walk.docName=walk.docName.toLowerCase();
             if(walk.docName.equals(docName)&&index==0){
                 walk=walk.nextDoc;
                 head.nextDoc=null;
@@ -27,6 +34,7 @@ public class DocsLinkedList {
                 prev.nextDoc=null;
             }else if(walk.docName.equals(docName)){
                 prev.nextDoc=walk.nextDoc;
+                walk.nextDoc=null;
                 return;
             }
             prev=walk;
@@ -35,14 +43,16 @@ public class DocsLinkedList {
         }
     }
     public String docsLinkedListToString(){
-        if(head==null) return null;
-        String docsString="";
         DocsNode walk=head;
+        if(walk==null){
+            return null;
+        }
+        String docsString="";
         while(walk!=null){
-            if(walk.nextDoc==null){
+            if(walk.nextDoc==null){//next null ise virgülsüz ekle
                 docsString+=walk.docName;
             }else{
-                docsString+=walk.docName+',';
+                docsString+=walk.docName+',';//daha devamında bişi gelicekse virgüllü ekle gibi yapmaya çalışmışım
             }
             walk=walk.nextDoc;
         }
@@ -51,7 +61,7 @@ public class DocsLinkedList {
     public boolean docsNameExist(String docName){
         if (head==null) return false;
         DocsNode walk=head;
-        while(walk.nextDoc!=null){
+        while(walk!=null){
             if(walk.docName==docName){
                 return true;
             }
--- src/QueryLinkedList.java
+++ src/QueryLinkedList.java
@@ -1,11 +1,12 @@
 import java.io.*;
+import java.util.HashSet;
 
 public class QueryLinkedList {
     QueryNode head;
     String outputFilePath="C:/Users/LENOVO/Desktop/output.txt";
 
-    public void load(String txtFilePath) throws IOException {
-        QueryLinkedList queryLinkedList = new QueryLinkedList();
+    public void load(String txtFilePath,QueryLinkedList queryLinkedList) throws IOException {
+
 
         String docName = null;
         boolean isFirstLine = true;
@@ -31,12 +32,14 @@ public class QueryLinkedList {
                     String[] words = line.split("\\s+");
                     for (String word : words) {
                         word = word.replaceAll("[^a-zA-Z]", "").toLowerCase();
-                        if (!queryWordExists(word)) {
-                            addNewQueryNode(word, docName);
-                        } else {
-                            QueryNode currentNode = get(word);
-                            if (!currentNode.docsLinkedList.docsNameExist(docName)) {
-                                currentNode.docsLinkedList.addNewDocNode(docName);
+                        if(!word.isEmpty()) {
+                            if (!queryWordExists(word)) {//science kelimesi yoksa eklicek. olup olmadığını iyi kontrol edemio sanırım.. yok ediomuş çünkü else kısmına geldi. demek ki science nodu var die
+                                queryLinkedList.addNewQueryNode(word, docName);
+                            } else {//science kelimesi varsa . o nodu alıp onun documan linked listinde var mı die bakıcak computer science. varsa eklemicek yoksa eklicek
+                                QueryNode currentNode = get(word);//sıkıntı burada ya da önümüzdeki 2 satırda
+                                if (!currentNode.docsLinkedList.docsNameExist(docName)) {
+                                    currentNode.docsLinkedList.addNewDocNode(docName);
+                                }
                             }
                         }
                     }
@@ -49,28 +52,57 @@ public class QueryLinkedList {
 
     public void search(String query){
         if(head==null) return;
+        HashSet<String>result=new HashSet<>();
+        String[] words=query.split(",");
         QueryNode walk=head;
-        while(walk!=null){
-            if(walk.wordValue.equals(query)){
-                try {
-                    FileWriter fileWriter=new FileWriter(outputFilePath);
-                    fileWriter.write("query "+query+"/n"+walk.docsLinkedList.docsLinkedListToString());
-                    System.out.println("query"+query+"/n"+walk.docsLinkedList.docsLinkedListToString());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
+        for(String word:words){
+            if(word.charAt(0)=='!'){//istemediğimiz kelime ise o kelime linkedlistimizde var mı die gezicez ve olduğu linked listleri
+                word=word.substring(1);
+                walk=head;
+                while(walk!=null){
+                    if(walk.wordValue.equals(word)){//mesela cat kelimesinin olmamasını istiyor cat kelimesini içeren bir node bulduysak. bunun documanlarını gezip hashsette varsa silicez
+                        String[]whatDocumentsThisWordAppear=walk.docsLinkedList.docsLinkedListToString().split(",");
+                        for(String docName:whatDocumentsThisWordAppear){
+                            if(result.contains(docName)){
+                                result.remove(docName);
+                            }
+                        }
+                        break;
+                    }
+                    walk=walk.nextWord;
                 }
-                return;
+            }else{//olmasını istediğimiz bi kelime ise
+                walk=head;
+                while(walk!=null){
+                    if(walk.wordValue.equals(word)){
+                        String[]whatDocumentsThisWordAppear=walk.docsLinkedList.docsLinkedListToString().split(",");
+                        for(String docName:whatDocumentsThisWordAppear){
+                            result.add(docName);
+                        }
+                        break;
+                    }
+                    walk=walk.nextWord;
+                }
             }
-            System.out.println("oi");
-            walk=walk.nextWord;
+        }
+        System.out.println("query " + query + "\n" + result+"\n");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath,true))) {//this opens the file in append mode. and with this way we are not overwriting output files content. we are adding new lines to it.
+            //output file boşsa direkt en başa yazabilirsin ama eğer içinde bir şeyler varsa en sonuna gelip sonuna eklemen lazım
+            writer.write("query " + query + "\n" + result);
+            writer.newLine();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     public void remove(String documentName){
         if(head==null) return;
+        documentName=documentName.toLowerCase();
         QueryNode walk=head;
         while(walk!=null){
-            walk.docsLinkedList.removeDocNode(documentName);
+            walk.docsLinkedList.removeDocNode(documentName);//her kelimenin documentlinkedlistinden çıkarmak istemişsin.öyle bi documentname varsa çıkarıcak yoksa da bişi yapmıcak artık
             walk=walk.nextWord;
         }
 
@@ -92,10 +124,10 @@ public class QueryLinkedList {
         DocsLinkedList newDocsLinkedList=new DocsLinkedList();
         newDocsLinkedList.addNewDocNode(docName);
         newQueryNode.docsLinkedList=newDocsLinkedList;
-        if(head==null){
-            head= newQueryNode;
+        if(this.head==null){
+            this.head= newQueryNode;
         }else{
-            QueryNode walk=head;
+            QueryNode walk=this.head;
             while(walk.nextWord!=null){
                 walk=walk.nextWord;
             }
